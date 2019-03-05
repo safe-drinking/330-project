61edfff7006bfd1020e3d572caab87265b7062cf
Hamilton <hamiltonvuu@gmail.com>
2019-03-04 19:40:20 -0600

    update cum

--- stats-page.js
+++ stats-page.js
@@ -9,6 +9,8 @@ var month = [6,8,7,5,7,4,5,6,4, localStorage.getItem("drinks")];
 var week = [3,4,5,8,9,9,5,4,2, localStorage.getItem("drinks")];
 var data_values = [year, month, week];
 var data_types = ['year', 'month', 'week'];
+var view_type = 2; // 0=>year, 1=>month, 2=>week
+var cum;
 
 var ctx = document.getElementById("myChart");
 var myChart = new Chart(ctx, {
@@ -50,6 +52,7 @@ function pillClick(num){
         document.getElementById(x).className = "inactive";
     });
     document.getElementById(pills[num]).className = "active";
+    view_type = document.getElementById(pills[num]).value;
 
     alerts.forEach(function(x){
         document.getElementById(x).innerHTML = data_types[num];
@@ -72,3 +75,7 @@ function removeData(chart) {
     chart.data.datasets.pop();
     chart.update();
 }
+
+function updateCum(){
+    cum = data_values[view_type].reduce(getSum);   
+} 
