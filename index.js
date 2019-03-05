c0b6ef4a5bcfd32a9615b289b2cd329da5acaa06
petersenea <Petersenea@sbcglobal.net>
2019-03-04 23:39:53 -0600

    can add friends to friends page

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var events = 0;
 var friends = 4;
+var friendsList = ["Amy", "Raul", "Mary", "Andrew"];
 
 function init(){
     localStorage.clear();
     localStorage.setItem("events", events);
     localStorage.setItem("friends", friends);
+    var i;
+    for(i=0;i<friends;i++){
+        localStorage.setItem("friend_"+i, friendsList[i]);
+
+    }
 }
 
 function login(){
