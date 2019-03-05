e1dc41f60da10c9a3c7fd6b4463a47ff46893cf3
petersenea <Petersenea@sbcglobal.net>
2019-03-05 16:39:11 -0600

    a bunch of stuff on event-page

--- event-page.js
+++ event-page.js
@@ -52,11 +52,6 @@ function addBuddy() {
     listFriends();
 }
 function shareBuddy() {
-    var i;
-    for (i = 0; i < friends; i++) {
-        var orange = document.getElementById("add_buddy_friend_" + i).value;
-    }
-
     closeFriendsModal();
     localStorage.setItem("event_" + eventName + "_buddy", currentClickedBuddy);
     document.getElementById("shared-friend-name").innerHTML = currentClickedBuddy;
@@ -70,7 +65,7 @@ function listFriends() {
         for (i = 0; i < friends; i++) {
             var buddyName = String(friendsList[i]);
             document.getElementById("add-buddy-friends-list").innerHTML +=
-                "<div class='radio'><label class='modal-opt'><input value='" + buddyName + "' onclick='handleRadioClick(this.value)' id='add_buddy_friend_" + i + "' type='radio' name='optradio'>" + buddyName + "</label></div>";
+                "<div class='radio'><label class='add-buddy-modal-opt'><input value='" + buddyName + "' onclick='handleRadioClick(this.value)' id='add_buddy_friend_" + i + "' type='radio' name='optradio'>" + buddyName + "</label></div>";
         }
     }
 }
