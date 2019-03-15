d49e36a52ff9d3bdcb5790de821f07f8e30f78db
Jacob Wat <jacobkwat@gmail.com>
2019-03-14 19:32:50 -0500

    fix user profile page

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-var gender = "Female";
+var gender = "";
 function init(){
     getName();
     getGender();
@@ -20,16 +20,28 @@ function getName(){
 }
 
 function getGender(){
-    if(Number(localStorage.getItem("gender")) == 1){ gender = "Male"; }
+    if (Number(localStorage.getItem("gender")) == 1){ 
+        gender = "Male";
+     } else if (Number(localStorage.getItem("gender")) == 0){
+         gender = "Female";
+     }
     document.getElementById("settings-gender").innerHTML = "Sex: " + gender;
 }
 
 function getHeight(){
+    if ((localStorage.getItem("height_foot") == "") || (localStorage.getItem("height_inch") == "")) {
+        document.getElementById("settings-height").innerHTML = "Height: ";
+    } else {
     document.getElementById("settings-height").innerHTML = "Height: " + localStorage.getItem("height_foot") + "'" + localStorage.getItem("height_inch") + "\"";
+    }
 }
 
 function getWeight(){
-    document.getElementById("settings-weight").innerHTML = "Weight: " + localStorage.getItem("weight") + " lb.";
+    if (localStorage.getItem("weight") == "") {
+        document.getElementById("settings-weight").innerHTML = "Weight: ";
+    } else {
+        document.getElementById("settings-weight").innerHTML = "Weight: " + localStorage.getItem("weight") + " lb.";
+    }
 }
 
 
